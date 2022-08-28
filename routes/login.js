f80fff94a00f2b2de240f0cb2d306b51645b8c85
Petya Angelova <bedfordpetya@gmail.com>
2022-08-28 18:18:19 +0300

    dashboardController added

--- routes/login.js
+++ routes/login.js
@@ -5,9 +5,12 @@ const {
   loginView
 } = require("../controllers/loginController");
 
+const { protectRoute} = require("../auth/protect");
+const { dashboardView } =require("../controllers/dashboardController")
 const router = express.Router();
 
 router.get("/register", registerView);
 router.get("/login", loginView);
+router.get("/dashboard", protectRoute, dashboardView);
 
 module.exports = router;
